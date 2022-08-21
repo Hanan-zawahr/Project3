1b90779e969f7076121a64e6f790e7c006103a95
Hanan Zawahra\thwaib <hanan.zawahra@harri.com>
2022-08-21 10:40:41 +0200

    Create Car class

---
+++ Git3/Car.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Car here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Car extends Vehicle
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    public int chair_num;
+    public boolean is_furnitre_leather;
+    
+    /**
+     * Constructor for objects of class Car
+     */
+    public Car()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
